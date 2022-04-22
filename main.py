d4eeda718ecb88a1a12e1d25ae02eb4518d482b7
SophiNewell <sophia.j.newell@gmail.com>
2022-04-22 11:14:51 -0500

    inserted user input and choice of pokemon

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pokebase as pb
+import random
+# check if user passed in a pokemon ID or name
+# if ID or name--get that pokemon
+# if not -- random
+user_pokemon = input()
+if input != "":
+    pokemon = pb.pokemon(user_pokemon)
+#else:
+#     pokemon = pb.pokemon(random.range(899))
+poke = pb.pokemon(898)
+
+# If pokemon has evolutions, link or suggest those
+# Create a UI
